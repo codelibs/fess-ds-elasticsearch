d6f8dc8bb45b73f537dec63be2d7e06b21e20d12
Ryo Kuramoto <kuramoto@n2sm.net>
2019-11-14 13:42:32 +0900

    Improve logger.debug()

--- src/main/java/org/codelibs/fess/ds/elasticsearch/ElasticsearchDataStore.java
+++ src/main/java/org/codelibs/fess/ds/elasticsearch/ElasticsearchDataStore.java
@@ -147,7 +147,7 @@ public class ElasticsearchDataStore extends AbstractDataStore {
 
                     if (logger.isDebugEnabled()) {
                         for (final Map.Entry<String, Object> entry : resultMap.entrySet()) {
-                            logger.debug(entry.getKey() + "=" + entry.getValue());
+                            logger.debug("{}={}", entry.getKey(), entry.getValue());
                         }
                     }
 
@@ -163,7 +163,7 @@ public class ElasticsearchDataStore extends AbstractDataStore {
 
                     if (logger.isDebugEnabled()) {
                         for (final Map.Entry<String, Object> entry : dataMap.entrySet()) {
-                            logger.debug(entry.getKey() + "=" + entry.getValue());
+                            logger.debug("{}={}", entry.getKey(), entry.getValue());
                         }
                     }
 
